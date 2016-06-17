c701a141f2be0fd6aafacfe32c054ec665c9869c
sxwailyc <shixiangwen03@gmail.com>
2016-06-17 11:27:25 +0800

    代码生成器

--- src/bage-coder/src/main/java/com/bageframework/coder/metadata/Metadata.java
+++ src/bage-coder/src/main/java/com/bageframework/coder/metadata/Metadata.java
@@ -1,5 +1,22 @@
 package com.bageframework.coder.metadata;
 
-public abstract class Metadata {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ModelMetadata extends BaseClassMetadata {
+
+	private List<Field> fields = new ArrayList<Field>();
+
+	public List<Field> getFields() {
+		return fields;
+	}
+
+	public void setFields(List<Field> fields) {
+		this.fields = fields;
+	}
+
+	public void appendField(Field field) {
+		fields.add(field);
+	}
 
 }
---
+++ src/bage-coder/src/main/java/com/bageframework/coder/service/CoderService.java
@@ -0,0 +1,8 @@
+package com.bageframework.coder.service;
+
+import com.bageframework.coder.core.Config;
+
+public interface CoderService {
+
+	public void generate(Config config, String table);
+}
